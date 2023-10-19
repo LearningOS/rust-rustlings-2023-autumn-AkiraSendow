2f32db4e9c7a7765224adf2b2f4120fc01427ebc
AkiraSendow <122533620+AkiraSendow@users.noreply.github.com>
2023-10-19 10:28:25 +0000

    option1-errors2

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -15,9 +15,10 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input);
+    //Ok(cost)
 
-    if cost > tokens {
+    if cost.expect() > tokens {
         println!("You can't afford that many!");
     } else {
         tokens -= cost;
